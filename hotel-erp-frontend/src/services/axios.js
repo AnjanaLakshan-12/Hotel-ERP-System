fe906e66fb17e5723c515bca3547022e7c8b6f7c
Deshapriya LGAL <lganjana12@gmail.com>
2026-07-13 05:25:15 +0530

    Update axios.js

--- hotel-erp-frontend/src/services/axios.js
+++ hotel-erp-frontend/src/services/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:8080",
+  baseURL: import.meta.env.VITE_API_BASE_URL || "https://otel-erp-backend-anjan-c6a9fraackccb2fs.southeastasia-01.azurewebsites.net",
   headers: {
     "Content-Type": "application/json"
   }
